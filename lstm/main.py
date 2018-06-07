6dc6fe6ff6b4b073ad0cc2cb595c3a0ffb7157da
EricAragorn <ericaragorncn@gmail.com>
2018-06-08 00:52:19 +0800

    Removed config.py: ModelConfig is now a class in main.py

--- lstm/main.py
+++ lstm/main.py
@@ -1,39 +1,65 @@
 from __future__ import division
 from __future__ import absolute_import
 
-from lstm import config
-
 import tensorflow as tf
 import pandas as pd
-import numpy as np
 import model
 
-class Market_Data:
-    def __init__(self):
-        # Read data from file
-        raw_data = pd.read_csv("../data/market_data.csv").fillna(0)
 
-        # Shuffle and create training/validation set
+class ModelConfig:
+    def __init__(self):
+        # features used for prediction and targets
+        self.feature_list = ["open", "high", "low", "close", "vwap", "volume", "count",
+                             "ask price5", "ask volume5", "ask price4", "ask volume4",
+                             "ask price3", "ask volume3", "ask price2", "ask volume2",
+                             "ask price1", "ask volume1", "bid price1", "bid volume1",
+                             "bid price2", "bid volume2", "bid price3", "bid volume3",
+                             "bid price4", "bid volume4", "bid price5", "bid volume5"]
+
+        self.target_list = ["high", "low"]
+        self.feature_size = len(self.feature_list)
+        self.target_size = len(self.target_list)
+
+        # Run configurations
+        self.batch_size = 100  # training batch size
+        self.num_steps = 60  # number of records feed into LSTM
+        self.time_lap = 30  # time lap between last record of input and target record
+        self.training_ratio = 0.75  # ratio of training set to data set
+        self.initial_lr = 0.05
+        self.lr_decay_rate = 0.8
+        self.max_epoches = 100
+        self.decay_range = 70  # start weight decay after epoch exceeds range
+
+        # LSTM settings
+        self.hidden_size = self.feature_size  # hidden units of a single LSTM cell
+        self.dropout_prob = 0.1  # dropout_
+        self.num_layers = 5
+
+
+class CryptoData:
+    # TODO: Modify this class to initialize with pandas Dataframe instead of file path
+    def __init__(self, path, model_config):
+        raw_data = pd.read_csv(path).fillna(0)
+
         split_loc = int(len(raw_data) * 0.75)
         self._training_set = raw_data.iloc[:split_loc, :]
         self._validation_set = raw_data.iloc[split_loc + 1:, :]
+        self._config = model_config
 
         # initialize batching state
         self._batch_base = 0
         self._finished = False
 
-    def next_training_batch(self, batch_size):
+    def next_training_batch(self, batch_size, num_steps, time_lap):
         if self._finished is True: raise BaseException("No more batches")
         batchX, batchY = [], []
-        time_lap = config.time_lap
         training_set = self._training_set
-        steps = config.num_steps
         base = self._batch_base
 
         for index in range(batch_size):
-            if base + index + steps + time_lap < len(training_set):
-                batchX.append(training_set.iloc[base + index: base + index + steps, :][config.feature_list].values)
-                batchY.append(training_set.iloc[base + index + steps + time_lap, :][config.target_list].values)
+            if base + index + num_steps + time_lap < len(training_set):
+                batchX.append(training_set.iloc[base + index: base + index + num_steps, :][self._config.feature_list].values)
+                batchY.append(training_set.iloc[base + index + num_steps + time_lap, :][self._config.target_list].values)
             else:
                 self._finished = True
                 break
@@ -41,58 +67,59 @@ class Market_Data:
         self._batch_base += batch_size
         return batchX, batchY
 
+    # WARNING: It is an incomplete version of fetching validation data
+    #          Currently it only fetches the first 100 samples from the validation set
+    #          Will be removed in the future
     def validation_set(self, batch_size):
         batchX, batchY = [], []
-        steps = config.num_steps
-        time_lap = config.time_lap
+        steps = self._config.num_steps
+        time_lap = self._config.time_lap
         v_set = self._validation_set
         for index in range(batch_size):
-            batchX.append(v_set.iloc[0 + index: 0 + index + steps, :][config.feature_list].values)
-            batchY.append(v_set.iloc[0 + index + steps + time_lap, :][config.target_list].values)
+            batchX.append(v_set.iloc[0 + index: 0 + index + steps, :][self._config.feature_list].values)
+            batchY.append(v_set.iloc[0 + index + steps + time_lap, :][self._config.target_list].values)
 
         return batchX, batchY
 
     def is_finished(self):
         return self._finished
 
-    # def small_batch(self):
-    #     X = np.reshape(self._training_set.iloc[:config.num_steps, :][config.feature_list].values, [1, config.num_steps, config.feature_size])
-    #     Y = np.reshape(self._training_set.iloc[config.num_steps + config.time_lap, :][config.target_list].values, [1, config.target_size])
-    #     return X, Y
-
     # Reset batching state
     def reset(self):
         self._batch_base = 0
         self._finished = False
 
-# def run_epoch(sess, model, config)
-
 
 def main(args):
-    data = Market_Data()
+    # TODO: Create an independent function for training & validation to improve readability
+    # TODO: Fix gradient vanishing problem after a certain epochs
+    model_config = ModelConfig()
+    data = CryptoData(path="../data/market_data.csv", model_config=model_config)
 
     with tf.Graph().as_default(), tf.Session() as sess:
-        m = model.LSTMModel(is_training=True)
+        m = model.LSTMModel(is_training=True, model_config=model_config)
         tf.global_variables_initializer().run()
         global_step = 0
-        for epoch in range(config.max_epoches):
-            lr_decay = config.lr_decay_rate ** max(epoch - config.decay_range, 0)
-            sess.run(m.update_lr, feed_dict={m._new_lr: lr_decay * config.initial_lr})
+        for epoch in range(model_config.max_epoches):
+            lr_decay = model_config.lr_decay_rate ** max(epoch - model_config.decay_range, 0)
+            sess.run(m.update_lr, feed_dict={m._new_lr: lr_decay * model_config.initial_lr})
             data.reset()
 
             train_loss = 0
             batch_count = 0
-            while data.is_finished() is False:
-                trainX, trainY = data.next_training_batch(config.batch_size)
-                if len(trainX) == config.batch_size:
+            while not data.is_finished():
+                trainX, trainY = data.next_training_batch(batch_size=model_config.batch_size,
+                                                          num_steps=model_config.num_steps,
+                                                          time_lap=model_config.time_lap)
+                if len(trainX) == model_config.batch_size:
                     _, _loss = sess.run([m.train_op, m.loss], feed_dict={m.input: trainX, m.target: trainY})
                     global_step += 1
                     train_loss += _loss
                     batch_count += 1
-                    print("Global Step:", global_step)
-            valX, valY = data.validation_set(config.batch_size)
-            _, val_loss = sess.run([m.output, m.loss], feed_dict={m.input: valX, m.target: valY})
-            print("Epoch: ", epoch, "Training Loss", train_loss/ batch_count, "Validation Loss: ", val_loss)
+            valX, valY = data.validation_set(model_config.batch_size)
+            val_loss = sess.run(m.loss, feed_dict={m.input: valX, m.target: valY})
+            print("Epoch: {:d}, Global steps: {:d}\n  Training Loss: {:.1f}, Validation Loss: {:.1f}"
+                  .format(epoch, global_step, train_loss / batch_count, val_loss))
 
 
 if __name__ == "__main__":
--- lstm/model.py
+++ lstm/model.py
@@ -1,57 +1,65 @@
 # Credit to Tensorflow authors: https://github.com/tensorflow/models/tree/master/tutorials/rnn/ptb
 
+"""
+An LSTM model to estimate scalar targets from time series
+"""
+
 import tensorflow as tf
-import config
 
 
 class LSTMModel:
-    def __init__(self, is_training):
-        self._hidden_size = config.hidden_size
-        self._num_steps = config.num_steps
-        self._batch_size = config.batch_size
+    def __init__(self, is_training, model_config):
+        self._hidden_size = model_config.hidden_size
+        self._num_steps = model_config.num_steps
+        self._batch_size = model_config.batch_size
         self._lr = tf.Variable(0.0, trainable=False)
 
-        self.input = tf.placeholder(tf.float32, [self._batch_size, self._num_steps, config.feature_size])
-        self.target = tf.placeholder(tf.float32, [self._batch_size, config.target_size])
+        self.input = tf.placeholder(tf.float32, [self._batch_size, self._num_steps, model_config.feature_size])
+        self.target = tf.placeholder(tf.float32, [self._batch_size, model_config.target_size])
 
         lstm_cell = tf.nn.rnn_cell.BasicLSTMCell(self._hidden_size, forget_bias=0.0, state_is_tuple=True, activation=tf.nn.relu)
-        if is_training and config.dropout_prob > 0:
-            lstm_cell = tf.nn.rnn_cell.DropoutWrapper(lstm_cell, output_keep_prob=1 - config.dropout_prob)
+        if is_training and model_config.dropout_prob > 0:
+            lstm_cell = tf.nn.rnn_cell.DropoutWrapper(lstm_cell, output_keep_prob=1 - model_config.dropout_prob)
 
-        cell = tf.nn.rnn_cell.MultiRNNCell([lstm_cell] * config.num_layers, state_is_tuple=True)
+        cell = tf.nn.rnn_cell.MultiRNNCell([lstm_cell] * model_config.num_layers, state_is_tuple=True)
         self._initial_state = cell.zero_state(batch_size=self._batch_size, dtype=tf.float32)
 
         inputs = self.input
-        if is_training and config.dropout_prob > 0:
-            inputs = tf.nn.dropout(inputs, 1 - config.dropout_prob)
+        if is_training and model_config.dropout_prob > 0:
+            inputs = tf.nn.dropout(inputs, 1 - model_config.dropout_prob)
 
-        with tf.variable_scope("lstm_output"):
+        with tf.variable_scope("lstm"):
             lstm_outputs = []
             state = self._initial_state
             for time_step in range(self._num_steps):
-                if time_step > 0: tf.get_variable_scope().reuse_variables()
+                if time_step > 0:
+                    tf.get_variable_scope().reuse_variables()
                 (output, state) = cell(inputs[:, time_step, :], state=state)
                 lstm_outputs.append(output)
 
             lstm_outputs = tf.concat(lstm_outputs, 1)
 
-        with tf.variable_scope("foo", reuse=False):
+        with tf.variable_scope("dense", reuse=False):
             h1 = tf.layers.dense(inputs=lstm_outputs,
                                  units=1024,
                                  activation=tf.nn.tanh,
                                  kernel_initializer=tf.initializers.truncated_normal(stddev=0.1),
                                  bias_initializer=tf.initializers.zeros(),
                                  name="hidden1")
+
+        with tf.variable_scope("dropout", reuse=False):
             d1 = tf.layers.dropout(inputs=h1,
-                                   rate=config.dropout_prob)
+                                   rate=model_config.dropout_prob)
 
-            self.output = tf.layers.dense(inputs=d1,
-                                          units=2,
-                                          kernel_initializer=tf.initializers.truncated_normal(stddev=0.1),
-                                          bias_initializer=tf.initializers.constant(0.1))
+        with tf.variable_scope("dense", reuse=False):
+            output = tf.layers.dense(inputs=d1,
+                                     units=model_config.target_size,
+                                     kernel_initializer=tf.initializers.truncated_normal(stddev=0.1),
+                                     bias_initializer=tf.initializers.zeros())
 
-            self.loss = tf.div(tf.reduce_sum(tf.square(self.output - self.target)), config.batch_size)
-            self.train_op = tf.train.AdamOptimizer(self._lr).minimize(self.loss)
+        # Loss function: squared error
+        self.loss = tf.div(tf.reduce_sum(tf.square(output - self.target)), model_config.batch_size)
+        self.train_op = tf.train.AdamOptimizer(self._lr).minimize(self.loss)
 
-            self._new_lr = tf.placeholder(tf.float32, [])
-            self.update_lr = tf.assign(self._lr, self._new_lr)
+        self._new_lr = tf.placeholder(tf.float32, [])
+        self.update_lr = tf.assign(self._lr, self._new_lr)
